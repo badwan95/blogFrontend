9bf95a5e222f654aabbb0d478b61c7ac78883436
Ammar <ammar.badwan95@gmail.com>
2020-09-18 07:19:22 +0300

    implemented the stories content

--- src/components/blog/storyPage.js
+++ src/components/blog/storyPage.js
@@ -1,33 +1,40 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import superagent from 'superagent';
+import {AuthenticationContext} from '../../auth/authContext';
+import NewContent from './newContent';
 
 const API = process.env.REACT_APP_API || 'http://localhost:8080';
 
 const StoryPage = props =>{
+  let context = useContext(AuthenticationContext);
+
   const [story,setStory] = useState({});
   const [content,setContent] = useState([]);
+  const [newPost,setNewPost] = useState(false);
+
   useEffect(()=>{
 
     superagent.get(`${API}/content/${props.id}`)
     .then(result=>{
-      console.log(result.body);
       setContent(result.body);
     })
     .catch(e=>{console.log(e)});
 
     superagent.get(`${API}/story/specific/${props.id}`)
     .then(result=>{
-      console.log(result.body);
       setStory(result.body);
     })
     .catch(e=>{console.log(e)});
     
-  },[props.id]);
+  },[props.id,newPost]);
 
   return(
     <>
+      {context.loggedin &&
+      <>
       <p>Story Title: {story.title}</p>
       <p>Created By: {story.owner}, On: {story.createdat}</p>
+      <NewContent newPost={newPost} setNewPost={setNewPost} storyid={story._id} token={context.token} />
       <p>Story Content: </p>
       {content.map((data,idx)=>{
         return(
@@ -38,7 +45,9 @@ const StoryPage = props =>{
           </section>
         )
       })}
-        <button onClick={e=>console.log(content)}>log</button>
+      </>
+      }
+      {!context.loggedin && <p>Please Sign in/up before accessing stories!</p>}
     </>
   )
 }
