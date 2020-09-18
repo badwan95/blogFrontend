3d50ffeca4912f282338d6462c321ebed4c86fcd
Ammar <ammar.badwan95@gmail.com>
2020-09-18 04:24:03 +0300

    added not found handler

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './app.scss';
 
 //Components
 import Header from './components/header/header';
 import Body from './components/body/body';
 import BlogPage from './components/blog/blogPage';
+import NotFound from './components/notFound/notFound';
 
-const App = props =>{
+const App = props => {
 
 
 
-  return(
+  return (
     <Router>
 
-    
-      <Header/>
-      <Route path="/" exact><Body/> </Route>
-      <Route path="/test" > testing new route</Route>
-      <Route path="/blog/:id" render={({match})=>(
-        <BlogPage id={match.params.id} />
-      )}/>
 
+      <Header />
+      <Switch>
+
+
+        <Route path="/" exact><Body /> </Route>
+        <Route path="/blog/:id" render={({ match }) => (
+          <BlogPage id={match.params.id} />
+        )} />
+
+        <Route component={NotFound} />
+
+      </Switch>
     </Router>
   )
 }
